5c9d41b7969bdf8a73998fe347771acde2a2ae1b
Tiger Tang <tigerhix@gmail.com>
2026-01-03 14:29:53 -0800

    Implement JLPT5: と-and (and particle)

--- packages/grammar/src/rules/bunpro/jlpt5/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt5/index.ts
@@ -23,6 +23,7 @@ import de from './で.js';
 import ni from './に.js';
 import he from './へ.js';
 import to from './と.js';
+import toAnd from './と-and.js';
 import yo from './よ.js';
 import ne from './ね.js';
 import ageru from './あげる.js';
@@ -73,6 +74,7 @@ export const BUNPRO_JLPT5: Ruleset = {
     ni,
     he,
     to,
+    toAnd,
     wo,
     wa,
     yo,
