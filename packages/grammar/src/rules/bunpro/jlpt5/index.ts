a33dd96392fdd685a6f076de29745a8ee9b3a530
Tiger Tang <tigerhix@gmail.com>
2026-01-03 12:24:25 -0800

    Implement JLPT5: に (dative particle)

--- packages/grammar/src/rules/bunpro/jlpt5/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt5/index.ts
@@ -8,10 +8,11 @@ import karaBecause from './から-because.js';
 import ga from './が.js';
 import gairu from './がいる.js';
 import kuru from './くる.js';
+import ni from './に.js';
 import shite from './して.js';
 import tekara from './てから.js';
 
 export const BUNPRO_JLPT5: Ruleset = {
   id: 'bunpro.jlpt5',
-  rules: [adjectivete, iAdjectives, ndesu, tsumorida, ka, karaBecause, ga, gairu, kuru, shite, tekara],
+  rules: [adjectivete, iAdjectives, ndesu, tsumorida, ka, karaBecause, ga, gairu, kuru, ni, shite, tekara],
 };
