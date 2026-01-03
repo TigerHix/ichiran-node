f2450e52b9fda96388d81280cb4d43233b22523c
Tiger Tang <tigerhix@gmail.com>
2026-01-03 11:36:02 -0800

    Fix index.ts: remove imports for unmerged rules

--- packages/grammar/src/rules/bunpro/jlpt5/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt5/index.ts
@@ -3,11 +3,8 @@ import adjectivete from './adjective-て-b.js';
 import iAdjectives from './い-adjectives.js';
 import ndesu from './-んです-のです.js';
 import tsumorida from './つもりだ.js';
-import kuru from './くる.js';
-import ageru from './あげる.js';
-import shite from './して.js';
 
 export const BUNPRO_JLPT5: Ruleset = {
   id: 'bunpro.jlpt5',
-  rules: [adjectivete, iAdjectives, ndesu, tsumorida, kuru, ageru, shite],
+  rules: [adjectivete, iAdjectives, ndesu, tsumorida],
 };
