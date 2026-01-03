0f51585e9d8b9552750b67e7eb12c6f89309a2da
Tiger Tang <tigerhix@gmail.com>
2026-01-03 10:30:54 -0800

    Implement JLPT5 rule: い-adjectives

--- packages/grammar/src/rules/bunpro/jlpt5/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt5/index.ts
@@ -1,9 +1,13 @@
 import type { Ruleset } from '../../../ruleset.js';
 import adjectivete from './adjective-て-b.js';
+import iAdjectives from './い-adjectives.js';
 import ndesu from './-んです-のです.js';
 import tsumorida from './つもりだ.js';
+import kuru from './くる.js';
+import ageru from './あげる.js';
+import shite from './して.js';
 
 export const BUNPRO_JLPT5: Ruleset = {
   id: 'bunpro.jlpt5',
-  rules: [adjectivete, ndesu, tsumorida],
+  rules: [adjectivete, iAdjectives, ndesu, tsumorida, kuru, ageru, shite],
 };
