910c3bdbb823e41acbb09101e09833f49911afa7
Tiger Tang <tigerhix@gmail.com>
2026-01-03 12:25:37 -0800

    Implement JLPT5: で (locative particle)

---
+++ packages/grammar/src/rules/bunpro/jlpt5/で.ts
@@ -0,0 +1,10 @@
+import { linguisticRule } from '../../../engine/lang.js';
+
+export default linguisticRule('で', (r) => {
+  // Locative/instrumental で particle (at/in/with/by means of)
+  // NOT conjunction で (copula te-form)
+  const de = r.particle('で', 'de', { dep: 'case' });
+  const noun = r.tok({ posOneOf: ['NOUN', 'PRON', 'DET', 'NUM'] }, 'noun');
+  r.caseMarker(noun, de);
+  r.capture(de);
+});
