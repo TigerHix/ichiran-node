f66bf3e1cbfd5112b43e34a1e655a7ea483a5278
Tiger Tang <tigerhix@gmail.com>
2026-01-03 16:12:42 -0800

    Clean up debug files

--- check-sentence.ts
+++
@@ -1,5 +0,0 @@
-import { loadBunproGrammarItemWithOptions } from './packages/grammar/src/data/bunpro/loader.js';
-
-const item = loadBunproGrammarItemWithOptions('./packages/grammar/data/bunpro/JLPT5/ている2.json', 'JLPT5', { allowTrivialSlug: true });
-const s = item.sentences.find((s: any) => s.sentence.includes('ならんでいる'));
-console.log(JSON.stringify(s, null, 2));
