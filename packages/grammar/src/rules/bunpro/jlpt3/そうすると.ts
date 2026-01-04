cec56bab608870f7e0e59b5d31876f2fe482b601
Tiger Tang <tigerhix@gmail.com>
2026-01-03 20:23:05 -0800

    Implement JLPT3: そうすると (and then)

---
+++ packages/grammar/src/rules/bunpro/jlpt3/そうすると.ts
@@ -0,0 +1,28 @@
+import { linguisticRule } from '../../../engine/lang.js';
+
+export default linguisticRule('そうすると', (r) => {
+  // そうすると (conjunction: "and then", "so", "with that")
+  // A sentence starter meaning "once (A) is done, (B)" or "upon (A), (B)"
+  // Similar to すると but with そう (like that/as such) added
+  //
+  // GiNZA may tokenize this as:
+  // 1. Single token: そうすると
+  // 2. Multiple tokens: そう (ADV) + する (VERB) + と (PART)
+
+  r.either(
+    // Branch 1: Single token (if GiNZA tokenizes it that way)
+    (b) => {
+      const sousuruto = b.tok({ text: 'そうすると' }, 'sousuruto');
+      b.capture(sousuruto);
+    },
+    // Branch 2: Multi-token: そう + する + と
+    (b) => {
+      const sou = b.tok({ text: 'そう' }, 'sou');
+      const suru = b.tok({ text: 'する' }, 'suru');
+      const to = b.tok({ text: 'と' }, 'to');
+      b.inOrder(sou, suru, 1);
+      b.inOrder(suru, to, 1);
+      b.captureSpan('そうすると', sou, to);
+    }
+  );
+});
