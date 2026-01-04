e6e2de6e449e24ebf593b7a829b2ce7f3481bc51
Tiger Tang <tigerhix@gmail.com>
2026-01-03 20:17:34 -0800

    Implement JLPT3: ずつ (each/every)

--- packages/grammar/src/rules/bunpro/jlpt3/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt3/index.ts
@@ -32,6 +32,7 @@ import kotoWaNai from './ことはない.js';
 import saCasualYo from './さ-casualよ.js';
 import dewaSoredewa from './では-それでは-じゃあ.js';
 import youToShinai from './-ようとしない.js';
+import zutsu from './ずつ.js';
 
 export const BUNPRO_JLPT3: Ruleset = {
   id: 'bunpro.jlpt3',
@@ -68,5 +69,6 @@ export const BUNPRO_JLPT3: Ruleset = {
     saCasualYo,
     dewaSoredewa,
     youToShinai,
+    zutsu,
   ],
 };
