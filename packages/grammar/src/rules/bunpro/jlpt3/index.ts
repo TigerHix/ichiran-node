0e0763382ff8584671e529257e0bd6df29cd988a
Tiger Tang <tigerhix@gmail.com>
2026-01-03 19:31:01 -0800

    Implement JLPT3: からこそ (precisely because)

--- packages/grammar/src/rules/bunpro/jlpt3/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt3/index.ts
@@ -1,8 +1,9 @@
 import type { Ruleset } from '../../../ruleset.js';
 import dewaSoredewa from './では-それでは-じゃあ.js';
 import youToShinai from './-ようとしない.js';
+import karaKoso from './からこそ.js';
 
 export const BUNPRO_JLPT3: Ruleset = {
   id: 'bunpro.jlpt3',
-  rules: [dewaSoredewa, youToShinai],
+  rules: [dewaSoredewa, youToShinai, karaKoso],
 };
