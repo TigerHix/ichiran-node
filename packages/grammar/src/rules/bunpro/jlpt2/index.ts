26341bb45a4cace20ede03150379a9312128eb72
Tiger Tang <tigerhix@gmail.com>
2026-01-04 16:02:03 -0800

    Implement JLPT2: しかしながら (however)

--- packages/grammar/src/rules/bunpro/jlpt2/index.ts
+++ packages/grammar/src/rules/bunpro/jlpt2/index.ts
@@ -1,9 +1,11 @@
 import type { Ruleset } from '../../../ruleset.js';
 import tekoso from './-てこそ.js';
 import nouchide from './-のうち-で.js';
+import shikamo from './しかも.js';
+import shikashinagara from './しかしながら.js';
+import zaru from './ざる.js';
 
 export const BUNPRO_JLPT2: Ruleset = {
   id: 'bunpro.jlpt2',
-  rules: [tekoso, nouchide],
+  rules: [tekoso, nouchide, shikamo, shikashinagara, zaru],
 };
-
